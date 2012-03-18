0eeeb2d3f33b5637f2baa7d71cdc105cb14a977a
Pavel Pozdniak <pozdpl@gmail.com>
2012-03-18 16:33:40 +1100

    Go to the new dojo AMD

--- html/js/index_audio.js
+++ html/js/index_audio.js
@@ -2,38 +2,32 @@
  * @author pozpl
  */
 
-dojo.addOnLoad(function() {
+require(["dojo/dom", 'dojo/query', "dojo/dom-class", "dojo/on","dojo/dom-attr", "dojo/mouse",
+"dojo/domReady!"], function(dom, query, domClass, on, domAttr) {
+
+//dojo.addOnLoad(function() {
 	var as = audiojs.createAll();
 	var audio = as[0];
-	dojo.query("ol > li > span[data-src]:first-child").forEach(function(node, index, arr) {
+	query("ol > li > span[data-src]:first-child").forEach(function(node, index, arr) {
 		//audio.load(dojo.attr(node, "data-src"));
 	});
 	//dojo.query("ol.playlist  > li:first-child").addClass("playing");
 
-	dojo.query("ol.playlist > li").connect("onclick", function(evt) {
-		dojo.query("ol.playlist > li.playing").removeClass("playing");
-		dojo.addClass(evt.currentTarget, "playing");
-
-		dojo.query("> span", evt.currentTarget).forEach(function(node, index, arr) {
-			audio.load(dojo.attr(node, "data-src"));
-			audio.play();
+	query("ol.playlist > li").on("click", function(evt) {
+		query("ol.playlist > li.playing").removeClass("playing");
+		domClass.add(this, "playing");
+		
+		query("> span", this).forEach(function(node, index, arr) {
+			audio.load(domAttr.get(node, "data-src"));
+			audio.play();			
 		});
 	});
 
-
+/*
 	soundManager.url = './jslib/soundmanager/';
 	soundManager.flashVersion = 9;
-	// optional: shiny features (default = 8)
 	soundManager.useFlashBlock = false;
-	// optionally, enable when you're ready to dive in
-	/*
-	 * read up on HTML5 audio support, if you're feeling adventurous.
-	 * iPad/iPhone and devices without flash installed will always attempt to use it.
-	 */
 	soundManager.onready(function() {
-
-		// SM2 has loaded - now you can create and play sounds!
-
 		var mySound = soundManager.createSound({
 			id : 'aSound',
 			url : './media/example.mp3'
@@ -44,4 +38,5 @@ dojo.addOnLoad(function() {
 		//mySound.play();
 
 	});
+*/
 });
